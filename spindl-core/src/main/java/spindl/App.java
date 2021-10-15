80b2ab1eb4c46f3b2c806425a02b1ffd85484752
KomodoTech <alexandre.leibler@gmail.com>
2021-10-14 21:50:51 -0400

    added to gitignore to exclude generated java files, and added a method to read file extension from filepath in main app

--- spindl-core/src/main/java/spindl/App.java
+++ spindl-core/src/main/java/spindl/App.java
@@ -4,10 +4,34 @@ package spindl;
  * Hello world!
  *
  */
+
+
 public class App 
 {
+    public String extractExtensionFromArgFile(String fileName)
+    {
+        
+        String[] names = fileName.split("\\.(?=[^\\.]+$)");
+        String fileExtension = names[1];
+        //System.out.println(fileExtension);
+
+        // for (int i = 0; i < names.length; i++) {
+        //     System.out.println("arg " + i + ": " + names[i]);
+        // }
+        return fileExtension;
+    }
+
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        for (int i = 0; i < args.length; i++) {
+            System.out.println("arg " + i + ": " + args[i]);
+        }
+        String fileName = args[0];
+
+        App myApp = new App(); //that's nutty
+        
+        String fileExtension = myApp.extractExtensionFromArgFile(fileName);
+        System.out.println(fileExtension);
     }
 }
