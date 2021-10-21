7d3f185faa6ef54ce73a27c68a53cf3c46fbd4d7
fortnight <fernando.ellis@gmail.com>
2021-10-21 18:21:53 -0400

    a branch to hole the spring boot api (mine is java 8)

---
+++ spindl-api/src/main/java/spindl/spindlapi/SpindlApiApplication.java
@@ -0,0 +1,13 @@
+package spindl.spindlapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpindlApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpindlApiApplication.class, args);
+	}
+
+}
