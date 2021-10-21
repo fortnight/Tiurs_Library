3e96f87054475893a6c65b845173f78a12c35aef
KomodoTech <alexandre.leibler@gmail.com>
2021-10-21 18:22:04 -0400

    a branch to hold the API generated by springboot with Java 17

---
+++ spindl-api/src/test/java/spindl/spindlapi/SpindlApiApplicationTests.java
@@ -0,0 +1,13 @@
+package spindl.spindlapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpindlApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
